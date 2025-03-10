c09208da4a4d6f3ac62cf813aa8f289edf07a91b
PetsaYuri <98687604+PetsaYuri@users.noreply.github.com>
2025-03-10 11:06:27 +0200

    Update index.ts
    fix path to 'public' folder

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,6 @@ app.listen(ENV.PORT, () => {
     console.log(`Server is running on port: ${ENV.PORT}`)
 })
 
-app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(pageRoute);
 app.use(authRoute);
